6e81bb22dec9d4b102d0be1d6fa15c01f0902795
JellyfishMIX <2273516339@qq.com>
2020-04-04 22:07:15 +0800

    :tada: Initial commit.

---
+++ src/main/java/com/jellyfishmix/wxinterchange/WxInterchangeApplication.java
@@ -0,0 +1,13 @@
+package com.jellyfishmix.wxinterchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxInterchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxInterchangeApplication.class, args);
+    }
+
+}
